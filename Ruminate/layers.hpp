2b491f7f9e5e52adfc8debe93eadf8545f2312e5
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-26 03:02:25 -0700

    :art:

--- Ruminate/layers.hpp
+++ Ruminate/layers.hpp
@@ -13,14 +13,8 @@ namespace rum
     public:
         using type = T;
 
-        //these vectors passing as ref is for performance reasons (safety issues come with it)
         virtual T ForwardProp(const T &) const = 0;
-        //virtual T BackProp(T &cost, std::vector<T> &backprops, std::vector<T> &forwardprops, size_t current_index) const = 0;
-        //virtual void Learn(std::vector<T> &backprops, size_t current_index) = 0;
     };
-
-    template <typename T>
-    concept LayerType = std::is_base_of<Layer<typename T::type>, T>::value;
 
     template <typename T>
     class IActivationFuncs
@@ -35,4 +29,6 @@ namespace rum
         IActivationFuncs(T (*a)(T), T (*ap)(T)) : Activation(a), ActivationPrime(ap) {}
     };
 
+    template <typename T>
+    concept LayerType = std::is_base_of<Layer<typename T::type>, T>::value;
 }; // namespace rum
