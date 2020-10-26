48f4d7f5abd7b3e4c1ad3a894c7641c41e268f9c
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-26 00:51:42 -0700

    namespace rum

--- Ruminate/net.hpp
+++ Ruminate/net.hpp
@@ -1,52 +1,57 @@
 #pragma once
 #include "layers.hpp"
 
-template <LayerType T>
-class NeuralNetwork
+namespace rum
 {
-private:
-    T **layers = nullptr;
-    size_t sz;
-    using RT = typename T::type; //representation type, for example: fMat
-
-public:
-    NeuralNetwork() = delete;
-    NeuralNetwork(auto &&... args) : sz(sizeof...(args))
+    template <LayerType T>
+    class NeuralNetwork
     {
-        layers = new T *[sz] { args... };
-    }
+    private:
+        T **layers = nullptr;
+        size_t sz;
+        using RT = typename T::type; //representation type, for example: fMat
 
-    std::vector<RT> ForwardProp(const RT &input) const
-    {
-        std::vector<RT> res(sz);
-        res[0] = layers[0]->ForwardProp(input);
-        for (size_t i = 1; i < res.size(); ++i)
+    public:
+        NeuralNetwork() = delete;
+        NeuralNetwork(auto &&... args) : sz(sizeof...(args))
         {
-            res[i] = layers[i]->ForwardProp(res[i - 1]); //result 1 = 1th layer propogated with 0th result
+            layers = new T *[sz] { args... };
         }
-        return res;
-    }
 
-    RT Cost(const RT &guess, const RT &anwser) const
-    {
-        RT res(guess.SizeX(), guess.SizeY());
-        for (size_t i = 0; i < guess.Area(); ++i)
+        std::vector<RT> ForwardProp(const RT &input) const
         {
-            res.FastAt(i) = 0.5 * ((guess.FastAt(i) - anwser.FastAt(i)) * (guess.FastAt(i) - anwser.FastAt(i)));
+            std::vector<RT> res(sz);
+            res[0] = layers[0]->ForwardProp(input);
+            std::cout << res[0] << '\n';
+            for (size_t i = 1; i < res.size(); ++i)
+            {
+                res[i] = layers[i]->ForwardProp(res[i - 1]); //result 1 = 1th layer propogated with 0th result
+                std::cout << res[i] << '\n';
+            }
+            return res;
         }
-        return res;
-    }
-    RT CostPrime(const RT &guess, const RT &anwser) const
-    {
-        return guess - anwser;
-    }
 
-    ~NeuralNetwork()
-    {
-        for (size_t i = 0; i < sz; ++i)
+        RT Cost(const RT &guess, const RT &anwser) const
         {
-            delete layers[i];
+            RT res(guess.SizeX(), guess.SizeY());
+            for (size_t i = 0; i < guess.Area(); ++i)
+            {
+                res.FastAt(i) = 0.5 * ((guess.FastAt(i) - anwser.FastAt(i)) * (guess.FastAt(i) - anwser.FastAt(i)));
+            }
+            return res;
         }
-        delete[] layers;
-    }
-};
+        RT CostPrime(const RT &guess, const RT &anwser) const
+        {
+            return guess - anwser;
+        }
+
+        ~NeuralNetwork()
+        {
+            for (size_t i = 0; i < sz; ++i)
+            {
+                delete layers[i];
+            }
+            delete[] layers;
+        }
+    };
+}; // namespace rum
