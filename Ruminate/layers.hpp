4956123ac62cab7c2d688ca8d0d3dffaf24da45e
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-26 00:52:05 -0700

    namespace rum

--- Ruminate/layers.hpp
+++ Ruminate/layers.hpp
@@ -3,31 +3,36 @@
 #include <type_traits>
 #include <vector>
 
-//an abstract type that is used polymorphically in net.h
-template <typename T>
-class Layer
+namespace rum
 {
-public:
-    using type = T;
 
-    //these vectors passing as ref is for performance reasons (safety issues come with it)
-    virtual T ForwardProp(const T &) const = 0;
-    //virtual T BackProp(T &cost, std::vector<T> &backprops, std::vector<T> &forwardprops, size_t current_index) const = 0;
-    //virtual void Learn(std::vector<T> &backprops, size_t current_index) = 0;
-};
+    //an abstract type that is used polymorphically in net.h
+    template <typename T>
+    class Layer
+    {
+    public:
+        using type = T;
 
-template <typename T>
-concept LayerType = std::is_base_of<Layer<typename T::type>, T>::value;
+        //these vectors passing as ref is for performance reasons (safety issues come with it)
+        virtual T ForwardProp(const T &) const = 0;
+        //virtual T BackProp(T &cost, std::vector<T> &backprops, std::vector<T> &forwardprops, size_t current_index) const = 0;
+        //virtual void Learn(std::vector<T> &backprops, size_t current_index) = 0;
+    };
 
-template <typename T>
-class IActivationFuncs
-{
-protected:
-    T (*Activation)
-    (T);
-    T (*ActivationPrime)
-    (T);
+    template <typename T>
+    concept LayerType = std::is_base_of<Layer<typename T::type>, T>::value;
 
-public:
-    IActivationFuncs(T (*a)(T), T (*ap)(T)) : Activation(a), ActivationPrime(ap) {}
-};
+    template <typename T>
+    class IActivationFuncs
+    {
+    protected:
+        T(*Activation)
+        (T);
+        T(*ActivationPrime)
+        (T);
+
+    public:
+        IActivationFuncs(T (*a)(T), T (*ap)(T)) : Activation(a), ActivationPrime(ap) {}
+    };
+
+}; // namespace rum
