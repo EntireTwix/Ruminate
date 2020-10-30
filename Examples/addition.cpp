ca3c7ee06ac6e575e1c5240a8537a84a7ec25cb0
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-30 15:43:27 -0700

    :bulb: comments

--- Examples/addition.cpp
+++ Examples/addition.cpp
@@ -27,14 +27,16 @@ int main()
         data.At(0, 1) = rng.nextUInt(100);
         anw.At(0, 0) = data.At(0, 0) + data.At(0, 1);
         std::cout << "Input:\n"
-            << data << '\n'
-            << "Anwser:\n"
-            << anw << '\n';
+                  << data << '\n'
+                  << "Anwser:\n"
+                  << anw << '\n';
         auto res = net.ForwardProp(data);
         std::cout << res.back() << "Cost:\n"
-            << net.Cost(res.back(), anw);
-        auto corrections = net.BackwordProp(res, res.back(), anw, 0.0001);
+                  << net.Cost(res.back(), anw);
+        auto corrections = net.BackwordProp(res, res.back(), anw, 0.00001);
         net.Learn(corrections);
+        
+        //hold enter to see training progress
         std::cin.get();
     }
 
