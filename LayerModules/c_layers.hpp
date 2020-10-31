4cfd81447437a0a6ad75ccf5df9914dd733bbe74
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-30 23:41:05 -0700

    :construction: working on convolution layers

---
+++ LayerModules/c_layers.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "../layers.hpp"
+#include "../../../OptimizedHeaders-main/mat.hpp"
+#include "../../../pcg32-master/pcg32.h"
+
+namespace rum
+{
+    class Convalution : public ImgMat
+    {
+    public:
+    };
+}; // namespace rum
