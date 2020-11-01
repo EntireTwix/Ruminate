1118c7a7a601277d33739c7af87d2660177a7b84
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-31 21:02:00 -0700

    :sparkles: loading functionality

--- Ruminate/LayerModules/ann_layers.hpp
+++ Ruminate/LayerModules/ann_layers.hpp
@@ -5,17 +5,22 @@
 #include "../../../OptimizedHeaders-main/mat.hpp"
 #include "../../../pcg32-master/pcg32.h"
 
+//weight
+//hidden
+//input
+//output
+
 namespace rum
 {
-    using FC = Layer<MLMat>;
+    using ANN = Layer<MLMat>;
 
-    class Weight : public FC
+    class Weight : public ANN
     {
     protected:
         MLMat weights;
 
     public:
-        Weight(uint16_t prev, uint16_t next, float w_min, float w_max, pcg32 &rng) : weights(prev, next)
+        Weight(uint16_t prev, uint16_t next, float w_min, float w_max, pcg32 &rng, auto &&... saved_params) : weights(prev, next, saved_params...)
         {
             for (uint16_t i = 0; i < weights.Area(); ++i)
             {
@@ -32,24 +37,28 @@ namespace rum
             //std::cout << "W\n";
             return weights.Dot(input);
         }
-        virtual MLMat BackwardProp(MLMat &cost, const std::vector<MLMat> &forwardRes, FC **layers, size_t index) const override
+        virtual MLMat BackwardProp(MLMat &cost, const std::vector<MLMat> &forwardRes, ANN **layers, size_t index) const override
         {
             //std::cout << "W\n";
             return cost.Dot(forwardRes[index - 1]);
         }
     };
 
-    class Hidden : public FC, public IActivationFuncs<float>
+    class Hidden : public ANN, public IActivationFuncs<float>
     {
     protected:
         MLMat biases;
 
     public:
-        Hidden(uint16_t hidden_nodes, float (*a)(float), float (*ap)(float), float b_min, float b_max, pcg32 &rng) : IActivationFuncs(a, ap), biases(hidden_nodes, 1)
+        Hidden(uint16_t hidden_nodes, float (*a)(float), float (*ap)(float), float b_min, float b_max, pcg32 &rng, auto &&... saved_params) : IActivationFuncs(a, ap), biases(hidden_nodes, 1, saved_params...)
         {
-            for (uint16_t i = 0; i < biases.Area(); ++i)
+            //only random init if saved params is empty
+            if (!sizeof...(saved_params))
             {
-                biases.FastAt(i) = (rng.nextFloat() * b_max) + b_min;
+                for (uint16_t i = 0; i < biases.Area(); ++i)
+                {
+                    biases.FastAt(i) = (rng.nextFloat() * b_max) + b_min;
+                }
             }
         }
 
@@ -70,14 +79,14 @@ namespace rum
             }
             return res;
         }
-        virtual MLMat BackwardProp(MLMat &cost, const std::vector<MLMat> &forwardRes, FC **layers, size_t index) const override
+        virtual MLMat BackwardProp(MLMat &cost, const std::vector<MLMat> &forwardRes, ANN **layers, size_t index) const override
         {
             //std::cout << "H\n";
             return cost = cost.Dot(layers[index + 1]->internal()) * forwardRes[index - 1].Transform(ActivationPrime); //to be optimized
         }
     };
 
-    class Input : public FC
+    class Input : public ANN
     {
     protected:
         MLMat inp;
@@ -98,7 +107,7 @@ namespace rum
     {
     public:
         Output(uint16_t hidden_nodes, float (*a)(float), float (*ap)(float), float b_min, float b_max, pcg32 &rng) : Hidden(hidden_nodes, a, ap, b_min, b_max, rng) {}
-        virtual MLMat BackwardProp(MLMat &cost, const std::vector<MLMat> &forwardRes, FC **layers, size_t index) const override
+        virtual MLMat BackwardProp(MLMat &cost, const std::vector<MLMat> &forwardRes, ANN **layers, size_t index) const override
         {
             //std::cout << "O\n";
             return cost = forwardRes[index].Transform(ActivationPrime) * cost; //to be optimized
