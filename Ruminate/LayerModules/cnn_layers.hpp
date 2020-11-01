c458bba2336a728ec794e47a4ab0f32bc439ac95
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-31 21:02:46 -0700

    :construction: conv_net still in progress

--- Ruminate/LayerModules/cnn_layers.hpp
+++ Ruminate/LayerModules/cnn_layers.hpp
@@ -3,10 +3,14 @@
 #include "../../../OptimizedHeaders-main/mat.hpp"
 #include "../../../pcg32-master/pcg32.h"
 
+//convolution
+//pooling
+
 namespace rum
 {
-    class Convalution : public ImgMat
+    using CNN = Layer<ImgMat>;
+
+    class Convolution : public CNN
     {
-    public:
     };
 }; // namespace rum
