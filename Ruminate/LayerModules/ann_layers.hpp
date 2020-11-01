44e117dc76225b67983755c04fc49885a7bcc4ad
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-31 23:21:21 -0700

    :sparkles: drop out for hidden layers

--- Ruminate/LayerModules/ann_layers.hpp
+++ Ruminate/LayerModules/ann_layers.hpp
@@ -48,17 +48,18 @@ namespace rum
     {
     protected:
         MLMat biases;
+        uint8_t dropThres;
+        pcg32 generator;
 
     public:
-        Hidden(uint16_t hidden_nodes, float (*a)(float), float (*ap)(float), float b_min, float b_max, pcg32 &rng, auto &&... saved_params) : IActivationFuncs(a, ap), biases(hidden_nodes, 1, saved_params...)
+        Hidden(uint16_t hidden_nodes, float (*a)(float), float (*ap)(float), float b_min, float b_max, uint8_t dropThres, pcg32 &rng, auto &&... saved_params) : IActivationFuncs(a, ap), biases(hidden_nodes, 1, saved_params...), dropThres(dropThres), generator(rng)
         {
             //only random init if saved params is empty
-            if (!sizeof...(saved_params))
+
+            for (uint16_t i = 0; i < biases.Area(); ++i)
             {
-                for (uint16_t i = 0; i < biases.Area(); ++i)
-                {
+                if constexpr (!sizeof...(saved_params))
                     biases.FastAt(i) = (rng.nextFloat() * b_max) + b_min;
-                }
             }
         }
 
@@ -70,11 +71,13 @@ namespace rum
         {
             //std::cout << "H\n";
             MLMat res(input.SizeX(), input.SizeY());
+
             for (uint16_t i = 0; i < input.SizeY(); ++i)
             {
                 for (uint16_t j = 0; j < input.SizeX(); ++j)
                 {
-                    res.At(j, i) = this->Activation(input.At(j, i) + biases.FastAt(j));
+                    res.At(j, i) = this->Activation(input.At(j, i) + biases.FastAt(j)) * (generator.nextUInt(100) > dropThres);
+                    std::cout << res.At(j, i) << '\n';
                 }
             }
             return res;
@@ -84,7 +87,7 @@ namespace rum
             //std::cout << "H\n";
             return cost = cost.Dot(layers[index + 1]->internal()) * forwardRes[index - 1].Transform(ActivationPrime); //to be optimized
         }
-    };
+    }; // namespace rum
 
     class Input : public ANN
     {
@@ -106,7 +109,7 @@ namespace rum
     class Output : public Hidden
     {
     public:
-        Output(uint16_t hidden_nodes, float (*a)(float), float (*ap)(float), float b_min, float b_max, pcg32 &rng) : Hidden(hidden_nodes, a, ap, b_min, b_max, rng) {}
+        Output(uint16_t hidden_nodes, float (*a)(float), float (*ap)(float), float b_min, float b_max, uint8_t dropThres, pcg32 &rng) : Hidden(hidden_nodes, a, ap, b_min, b_max, dropThres, rng) {}
         virtual MLMat BackwardProp(MLMat &cost, const std::vector<MLMat> &forwardRes, ANN **layers, size_t index) const override
         {
             //std::cout << "O\n";
