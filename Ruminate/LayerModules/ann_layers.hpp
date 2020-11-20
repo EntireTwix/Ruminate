bc40e00dd63d8e7ae6ae36d8b6dc9e11a35e7d03
William Katz <qualitycontentquestionmark@gmail.com>
2020-11-20 13:41:54 -0800

    made input and dropout generic, and added batch layer

--- Ruminate/LayerModules/ann_layers.hpp
+++ Ruminate/LayerModules/ann_layers.hpp
@@ -8,23 +8,24 @@ namespace rum
 {
     using ANN = Layer<MLMat>;
 
-    class Input : public ANN
+    template <Matrix M>
+    class Input : public Layer<M>
     {
     protected:
-        MLMat inp;
+        M inp;
 
     public:
         Input(uint16_t input_sz) : inp(1, input_sz) {}
-        virtual MLMat &internal()
+        virtual M &internal()
         {
             return inp;
         }
-        virtual MLMat ForwardProp(const MLMat &input) override
+        virtual M ForwardProp(const M &input) override
         {
             //std::cout << "I\n";
-            return inp = input;
+            return inp = input; //unfortunately a copy call
         }
-        virtual void Learn(const MLMat &correction) override {} //doesnt correct
+        virtual void Learn(const M &correction) override {} //doesnt correct
     };
 
     class Weight : public ANN
@@ -105,29 +106,52 @@ namespace rum
         }
     };
 
-    class DropOut : public ANN
+    template <Matrix M>
+    class DropOut : public Layer<M>
     {
     private:
-        MLMat t_vals;
+        M t_vals;
         float thres;
 
     public:
         DropOut(uint16_t sz, float thres) : t_vals(sz, 1), thres(thres) {}
-        virtual MLMat ForwardProp(const MLMat &input) override
+        virtual M ForwardProp(const M &input) override
         {
-            MLMat res(input.SizeX(), input.SizeY());
-            for (uint32_t i = 0; i < input.Area(); ++i)
+            M res(input.SizeX(), input.SizeY());
+            for (size_t i = 0; i < input.Area(); ++i)
             {
                 res.FastAt(i) = input.FastAt(i) * (t_vals.FastAt(i) = gen.nextFloat() > thres);
             }
             return res;
         }
-        virtual MLMat BackwardProp(MLMat &cost, const std::vector<MLMat> &forwardRes, ANN **layers, size_t index) const override
+        virtual M BackwardProp(M &cost, const std::vector<M> &forwardRes, ANN **layers, size_t index) const override
         {
             cost *= t_vals;
-            return MLMat(); //as MLMat doesnt utilize/isnt utilized by other layers
+            return M(); //as MLMat doesnt utilize/isnt utilized by other layers
         }
-        virtual MLMat &internal() override { return t_vals; }
-        virtual void Learn(const MLMat &correction) override {} //doesnt correct
+        virtual M &internal() override { return t_vals; }
+        virtual void Learn(const M &correction) override {} //doesnt correct
+    };
+
+    template <Matrix M>
+    class Batch : public Input<M>
+    {
+    private:
+        uint8_t batch_sz;
+
+    public:
+        Batch(uint8_t batch_sz, uint16_t input_sz) : batch_sz(batch_sz), Input<M>(input_sz) {}
+        virtual M ForwardProp(const M &input) override
+        {
+            for (typename M::storage_type i = 0; i < input.SizeY(); ++i)
+            {
+                for (typename M::storage_type j = 0; j < input.SizeX(); ++j)
+                {
+                    this->inp.At(0, i) += input.At(j, i);
+                }
+                this->inp.At(0, i) /= input.SizeX();
+            }
+            return this->inp;
+        }
     };
 } // namespace rum
