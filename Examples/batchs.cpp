55cfdc4c953d52bb3ab2cfe261b349395b5a7e08
William Katz <qualitycontentquestionmark@gmail.com>
2020-11-20 13:34:25 -0800

    :bulb: batch comments

--- Examples/batchs.cpp
+++ Examples/batchs.cpp
@@ -10,10 +10,10 @@ int main()
 {
 
     NeuralNetwork<ANN> net{
-        new Batch(10, 2),
-        new Weight(2, 3, new RngInit()), //2x1 weights
-        new Hidden(3, ReluLeaky, ReluLeakyPrime),
-        new Weight(3, 1, new RngInit()),
+        new Batch(10, 2),                         //batch input of 10
+        new Weight(2, 3, new RngInit()),          //2x1 weights
+        new Hidden(3, ReluLeaky, ReluLeakyPrime), //3 hidden nodes
+        new Weight(3, 1, new RngInit()),          //3x1 weights
         new Output(1, ReluLeaky, ReluLeakyPrime), //1 output node
     };
 
